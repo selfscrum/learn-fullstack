88badc0ff4689029bc7cdb24b26764e1a05ad0da
Selfscrum <martin@selfscrum.com>
2022-03-26 19:16:46 +0100

    part1-5

---
+++ part1/course-information/1.3-course-information-step3.js
@@ -0,0 +1,45 @@
+const Header = (props) => (
+    <h1>{props.course}</h1>
+  )
+  
+  const Content = (props) => (
+    <>
+      <p> {props.part1} {props.exercises1}</p>
+      <p> {props.part2} {props.exercises2}</p>
+      <p> {props.part3} {props.exercises3}</p>
+    </>
+  )
+  
+  const Total = (props) => (
+    <p>Number of exercises {props.total}</p>
+  )
+  
+  const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+    name: 'State of a component',
+    exercises: 14
+    }
+  
+    return (
+      <>
+        <Header course = {course}/>
+        <Content part1={part1.name} exercises1={part1.exercises} 
+                 part2={part2.name} exercises2={part2.exercises}
+                 part3={part3.name} exercises3={part3.exercises}
+        />
+        <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      </>
+    );
+  }
+  
+  export default App;
+  
